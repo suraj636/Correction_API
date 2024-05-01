7e0970969f2daf154fbc60dced822537607e5fe4
Suraj Kumar Singh <75930198+suraj636@users.noreply.github.com>
2024-05-01 12:25:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from pymongo import MongoClient
+
+app = FastAPI()
+
+# Connect to MongoDB
+client = MongoClient("mongodb://localhost:27017/")
+db = client["SpeechToText"]
+collection = db["Correction"]
+
+# Define CORS policy
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
+
+# Define data models using Pydantic
+class Correction(BaseModel):
+    selected_words: list[str]
+    corrected_sentence: str
+
+# Default route
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome to the Custom API!"}
+
+
+# Define API endpoints
+@app.post("/submit_correction")
+async def submit_correction(correction: Correction):
+    # Insert data into MongoDB
+    inserted_data = collection.insert_one({
+        "selected_words": correction.selected_words,
+        "corrected_sentence": correction.corrected_sentence
+    })
+    return {"message": "Correction submitted successfully", "inserted_id": str(inserted_data.inserted_id)}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
