60dfbbb47bc498be2952ccf49e25564fac9a41ca
Suraj Kumar Singh <75930198+suraj636@users.noreply.github.com>
2024-05-01 13:22:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+import os
+
 
 app = FastAPI()
 
