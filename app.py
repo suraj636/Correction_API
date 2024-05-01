804f498ec0d08cc089be5aa8cd8035e83f1164ca
Suraj Kumar Singh <75930198+suraj636@users.noreply.github.com>
2024-05-01 13:18:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import os
 from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 
 app = FastAPI()
 
-url=os.getenv("MONGODB_URI")
-# Connect to MongoDB
-client = MongoClient(url)
+# MongoDB Atlas connection string
+uri = os.getenv("MONGODB_URI")
+
+# Connect to MongoDB Atlas
+client = MongoClient(uri, server_api=ServerApi('1'))
 db = client["SpeechToText"]
-collection = db["Correction"]
+collection = db["Correction_Feedback"]
 
 # Define CORS policy
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -32,17 +33,15 @@ class Correction(BaseModel):
 async def read_root():
     return {"message": "Welcome to the Custom API!"}
 
-
 # Define API endpoints
 @app.post("/submit_correction")
 async def submit_correction(correction: Correction):
-    # Insert data into MongoDB
+    # Insert data into MongoDB Atlas
     inserted_data = collection.insert_one({
         "selected_words": correction.selected_words,
         "corrected_sentence": correction.corrected_sentence
     })
     return {"message": "Correction submitted successfully", "inserted_id": str(inserted_data.inserted_id)}
-
 
 if __name__ == "__main__":
     import uvicorn
